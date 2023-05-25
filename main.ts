1b464310e75273aec60cc295f42aeda27c58d644
Wagner <wprodriguesbr@gmail.com>
2023-05-25 10:30:21 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onGesture(Gesture.ThreeG, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
